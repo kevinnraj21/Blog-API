bff44e45309f9aa038e0ce5c17fa7ba62559f408
kevinraj21 <karanraj827010@gmail.com>
2024-01-21 00:19:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,15 +38,13 @@ let lastId = 3;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Write your code here//
-
-//CHALLENGE 1: GET All posts
+//GET All posts
 app.get("/posts", (req,res)=>{
   console.log(posts);
   res.json(posts);
 });
 
-//CHALLENGE 2: GET a specific post by id
+//GET a specific post by id
 app.get("/posts/:id", (req,res)=>{
   const id = parseInt(req.params.id);
   const post = posts.find((item)=> item.id === id);
@@ -56,7 +54,7 @@ app.get("/posts/:id", (req,res)=>{
     res.status(404).json({error:`Post with id: ${id} not found.`});
 })
 
-//CHALLENGE 3: POST a new post
+//POST a new post
 app.post("/posts", (req,res)=>{
   const newPost = {
     id: lastId+1,
@@ -70,7 +68,7 @@ app.post("/posts", (req,res)=>{
   res.status(201).json(newPost);
 });
 
-//CHALLENGE 4: PATCH a post when you just want to update one parameter
+//PATCH a post when you just want to update one parameter
 app.patch("/posts/:id", (req,res)=>{
   const id = parseInt(req.params.id);
   const post = posts.find((item)=> item.id===id);
@@ -88,7 +86,7 @@ app.patch("/posts/:id", (req,res)=>{
   res.status(200).json(updatedPost);
 })
 
-//CHALLENGE 5: DELETE a specific post by providing the post id.
+//DELETE a specific post by providing the post id.
 app.delete("/posts/:id", (req,res)=>{
   const id = parseInt(req.params.id);
   const index = posts.findIndex((item)=> item.id === id);
